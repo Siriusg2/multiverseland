682e86485d05c9c5e84ab47258e1abaf4f669750
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2022-11-24 16:42:40 -0500

    modified prop onclose to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import styles from "./Cards.module.css";
 export default function Card(props) {
    return (
       <div className={styles.divCard}>
-         <button onClick={props.onClose} className={styles.button}>X</button>
+         <button onClick={ () => window.alert("Emulamos que se cierra la card")  } className={styles.button}>X</button>
           <img  src={props.image} alt="" />    
            <h2  className={styles.firstText}>{props.name}</h2>
     
--- src/data.js
+++ src/data.js
@@ -4,28 +4,24 @@ const characters = [
     species: "Human",
     gender: "Male",
     image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    onClose: () => window.alert("Emulamos que se cierra la card"),
   },
   {
     name: "Morty Smith",
     species: "Human",
     gender: "Male",
     image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    onClose: () => window.alert("Emulamos que se cierra la card"),
   },
   {
     name: "Summer Smith",
     species: "Human",
     gender: "Female",
     image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-    onClose: () => window.alert("Emulamos que se cierra la card"),
   },
   {
     name: "Beth Smith",
     species: "Human",
     gender: "Female",
     image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-    onClose: () => window.alert("Emulamos que se cierra la card"),
   },
 ];
 
